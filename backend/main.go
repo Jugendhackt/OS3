d48134c1c582994213a37ff6fe3714a48f8ce831
noa1510 <14131388+noah1510@users.noreply.github.com>
2018-06-16 13:35:26 +0200

    updated/upgraded Server backend
    Signed-off-by: noa1510 <14131388+noah1510@users.noreply.github.com>

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
-    // "fmt"
-    // "io"
-    "net/http"
     "log"
+    "crypto/tls"
+    "net"
+    "bufio"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "text/plain")
-    w.Write([]byte("This is an example server.\n"))
-    // fmt.Fprintf(w, "This is an example server.\n")
-    // io.WriteString(w, "This is an example server.\n")
+func main() {
+    log.SetFlags(log.Lshortfile)
+
+    cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    config := &tls.Config{Certificates: []tls.Certificate{cer}}
+    ln, err := tls.Listen("tcp", ":443", config) 
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    defer ln.Close()
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            log.Println(err)
+            continue
+        }
+        go handleConnection(conn)
+    }
 }
 
-func main() {
-    http.HandleFunc("/hello", HelloServer)
-    err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
+func handleConnection(conn net.Conn) {
+    defer conn.Close()
+    r := bufio.NewReader(conn)
+    for {
+        msg, err := r.ReadString('\n')
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        println(msg)
+
+        n, err := conn.Write([]byte("world\n"))
+        if err != nil {
+            log.Println(n, err)
+            return
+        }
     }
 }
