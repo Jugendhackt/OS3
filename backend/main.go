92e68208d63fcb2dd1fef70ff0b27ced070d6736
noah1510 <14131388+noah1510@users.noreply.github.com>
2018-06-19 20:23:14 +0200

    Revert "Moved logindata to login.json"
    This reverts commit 01796bc2854c687f1897eb89aeb80c9dc4d9d9d0.

--- backend/main.go
+++ backend/main.go
@@ -16,30 +16,17 @@ import (
 var database *sql.DB
 
 func main() {
-	
+	username := "root"
+	passsword := "testdb"
+	address := "localhost"
+	dbname := "OS3"
+	charset := "uft8"
 
-	logindata, err := ioutil.ReadFile("./login.json")
-
-	var login struct {
-		username    string    `json:"username"`
-		passsword string `json:"passsword"`
-		address   string `json:"address"`
-		dbname   string `json:"dbname"`
-		charset   string `json:"charset"`
-	}
-
-    json.Unmarshal(raw, &login)
-
+	logindata, err := ioutil.ReadFile("login.json")
 	if err != nil {
-		login.username = "root"
-		login.passsword = "root"
-		login.address = "localhost"
-		login.dbname = "OS3"
-		login.charset = "uft8"
-
 		fmt.Println(err.Error())
 	} else {
-		fmt.Println(login)
+		fmt.Println(logindata)
 	}
 
 	//Openng the Connection to the mysql data base,
